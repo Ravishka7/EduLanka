9acc27c7c4f27488ff587e5914e8e9a2daf7047f
Ravishka7 <107683747+Ravishka7@users.noreply.github.com>
2023-08-05 23:04:11 +0530

    project

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('admin',function(){
+    return view('admin');
+})->name('admin')->middleware('admin');
+
+Route::get('principal',function(){
+    return view('principal');
+})->name('principal')->middleware('principal');
+
+Route::get('teacher',function(){
+    return view('teacher');
+})->name('teacher')->middleware('teacher');
+
+Route::get('student',function(){
+    return view('student');
+})->name('student')->middleware('student');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
